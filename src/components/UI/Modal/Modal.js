63ccea9416a3a79cefc79aaaaa7bd0cff70d809c
Ayushi Sijaria <ayushi.sijaria@wizroots.com>
2022-07-05 12:09:14 +0530

    till 99

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,3 +1,25 @@
-import "./Modal.css";
-const Modal = () => {};
+import Button from "../Button/Button";
+import Card from "../Card/Card";
+import classes from "./Modal.module.css";
+const Modal = (props) => {
+  const closeModalHandler = () => {
+    props.onClick();
+  };
+  return (
+    <div onClick={props.onClick}>
+      <div className={classes.backdrop}></div>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>Invalid Form</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onclick={closeModalHandler}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
 export default Modal;
--- src/components/Users/AddUsers/AddUsers.js
+++ src/components/Users/AddUsers/AddUsers.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
+import Modal from "../../UI/Modal/Modal";
 import classes from "./AddUsers.module.css";
 
 const AddUsers = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        message: "Please enter valid input(non-empty values).",
+      });
       return;
-    }
-    if (+enteredAge < 1) {
+    } else if (+enteredAge < 1) {
+      setError({
+        message: "Age should be greater than 0.",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -26,31 +33,39 @@ const AddUsers = (props) => {
   const ageChangeHandler = (e) => {
     setEnteredAge(e.target.value);
   };
+  const closeModal = () => {
+    setError(null);
+  };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={submitFormHandler}>
-        <div>
-          <label htmlFor="username">Name:</label>
-          <input
-            type="text"
-            id="username"
-            onChange={usernameChangeHandler}
-            value={enteredUsername}
-          />
-        </div>
-        <div>
-          <label htmlFor="age">Age:</label>
-          <input
-            type="number"
-            id="age"
-            onChange={ageChangeHandler}
-            value={enteredAge}
-          />
-        </div>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && <Modal message={error.message} onClick={closeModal} />}
+      <Card className={classes.input}>
+        <form onSubmit={submitFormHandler}>
+          <div>
+            <label htmlFor="username">Name:</label>
+            <input
+              type="text"
+              id="username"
+              onChange={usernameChangeHandler}
+              value={enteredUsername}
+            />
+          </div>
+          <div>
+            <label htmlFor="age">Age:</label>
+            <input
+              type="number"
+              id="age"
+              onChange={ageChangeHandler}
+              value={enteredAge}
+            />
+          </div>
+          <Button type="submit" onClick={closeModal}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 export default AddUsers;
