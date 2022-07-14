fe708aa6b163999b7930045299e078a2444a52d6
Ayushi Sijaria <ayushi.sijaria@wizroots.com>
2022-07-14 22:24:03 +0530

    till 108

--- src/components/Users/AddUsers/AddUsers.js
+++ src/components/Users/AddUsers/AddUsers.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
 import Modal from "../../UI/Modal/Modal";
 import classes from "./AddUsers.module.css";
 
 const AddUsers = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
+
+  const nameRef = useRef();
+  const ageRef = useRef();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+    const enteredUsername = nameRef.current.value;
+    const enteredAge = ageRef.current.value;
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         message: "Please enter valid input(non-empty values).",
@@ -23,16 +26,10 @@ const AddUsers = (props) => {
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    nameRef.current.value = "";
+    ageRef.current.value = "";
   };
 
-  const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
-  };
   const closeModal = () => {
     setError(null);
   };
@@ -44,21 +41,11 @@ const AddUsers = (props) => {
         <form onSubmit={submitFormHandler}>
           <div>
             <label htmlFor="username">Name:</label>
-            <input
-              type="text"
-              id="username"
-              onChange={usernameChangeHandler}
-              value={enteredUsername}
-            />
+            <input type="text" id="username" ref={nameRef} />
           </div>
           <div>
             <label htmlFor="age">Age:</label>
-            <input
-              type="number"
-              id="age"
-              onChange={ageChangeHandler}
-              value={enteredAge}
-            />
+            <input type="number" id="age" ref={ageRef} />
           </div>
           <Button type="submit" onClick={closeModal}>
             Add User
